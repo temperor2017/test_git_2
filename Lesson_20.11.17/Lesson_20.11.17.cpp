61bae4a46d1d284ff06354459757634139bacee0
temperor2017 <temperor2008@gmail.com>
2017-11-20 19:34:28 +0200

    Hello

---
+++ Lesson_20.11.17/Lesson_20.11.17.cpp
@@ -0,0 +1,19 @@
+// Lesson_20.11.17.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	cout << "first come it"<<endl;
+
+
+	
+	return 0;
+}
+
